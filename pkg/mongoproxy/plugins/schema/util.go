f15bf23e3945af06b6cb2c17093a3fd78b6b8592
yinwang-wish <92184841+yinwang-wish@users.noreply.github.com>
2022-10-26 17:18:41 -0700

    update

--- pkg/mongoproxy/plugins/schema/util.go
+++ pkg/mongoproxy/plugins/schema/util.go
@@ -3,11 +3,12 @@ package schema
 import (
 	"context"
 	"fmt"
+	"reflect"
+	"regexp"
+
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"reflect"
-	"regexp"
 )
 
 var (
