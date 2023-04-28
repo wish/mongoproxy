9fa692c8ae611f0f5c0ca6ab6398115851fa60d5
hzhao-wish <hzhao@wish.com>
2023-04-28 15:17:18 -0700

    fix integration test

--- integrationtest/jstests/basic1.js
+++ integrationtest/jstests/basic1.js
@@ -6,16 +6,11 @@ t.drop();
 o = {
     a: 1
 };
-t.save(o);
+t.insertOne(o);
+assert(1==t.findOne().a, "first")
 
-assert.eq(1, t.findOne().a, "first");
-assert(o._id, "now had id");
-assert(o._id.str, "id not a real id");
-
-o.a = 2;
-t.save(o);
-
-assert.eq(2, t.findOne().a, "second");
+t.updateOne({ a: 1 }, { $set: { a: 2 } })
+assert(2==t.findOne().a, "second")
 
 // not a very good test of currentOp, but tests that it at least
 // is sort of there:
