91778835a8c94e3fccdaaad2ed4d57a7a7f75f4d
hzhao-wish <hzhao@wish.com>
2023-04-28 14:53:02 -0700

    test

--- integrationtest/jstests/basic1.js
+++ integrationtest/jstests/basic1.js
@@ -9,9 +9,9 @@ o = {
 t.insertOne(o);
 assert(1==t.findOne().a, "first")
 
-t.updateOne({ a: 1 }, { $set: { a: 2 } })
-assert(2==t.findOne().a, "second")
+// t.updateOne({ a: 1 }, { $set: { a: 2 } })
+// assert(2==t.findOne().a, "second")
 
-// not a very good test of currentOp, but tests that it at least
-// is sort of there:
-assert(db.currentOp().inprog != null);
+// // not a very good test of currentOp, but tests that it at least
+// // is sort of there:
+// assert(db.currentOp().inprog != null);
