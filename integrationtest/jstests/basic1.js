8f853c5a1ead896f29a4ba2c8dddcd2075e3bc1d
hzhao-wish <hzhao@wish.com>
2023-04-28 14:36:21 -0700

    test

--- integrationtest/jstests/basic1.js
+++ integrationtest/jstests/basic1.js
@@ -7,14 +7,10 @@ o = {
     a: 1
 };
 t.insertOne(o);
-
 assert.eq(1, t.findOne().a, "first");
-assert(o._id, "now had id");
-assert(o._id.str, "id not a real id");
 
 o.a = 2;
-t.insertOne(o);
-
+t.update({a:1}, o)
 assert.eq(2, t.findOne().a, "second");
 
 // not a very good test of currentOp, but tests that it at least
